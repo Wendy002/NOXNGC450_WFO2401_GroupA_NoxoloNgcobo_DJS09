2952e2c599b7f48c45a486090bef01b3ff7bc823
Noxolo <wendyngcobo98@gmail.com>
2024-06-06 15:38:32 +0200

    add comments to utils.ts

--- djs09/src/utils.ts
+++ djs09/src/utils.ts
@@ -1,5 +1,13 @@
 import { LoyaltyUser } from "./enum.ts"
 
+/* showReviewTota Function to display review total and reviewer information
+   Determine whether the user is a gold user (loyalty status)
+   Update the reviewTotalDisplay element's content
+   populateUser Function to populate user-related information
+   If the user is returning, display "back" in the returningUserDisplay element
+   isplay the user's name in the userNameDisplay element
+   */
+
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
