d83116f032429db090c3c2b124fbcf02f2c97761
Noxolo <wendyngcobo98@gmail.com>
2024-06-06 08:32:28 +0200

    converted ts file to js file

--- index.js
+++ index.js
@@ -1,12 +1,13 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
-exports.reviewTotalDisplay = void 0;
 // Number Types mini-challenge 10 10.2
 // Write a function that will only accept numbers and attend to 
 // all TypeScript weakness flags.
 // : number
-exports.reviewTotalDisplay = document.querySelector('#reviews');
-var isLoyal = false;
+var utils_1 = require("./utils");
+var isOpen;
+var propertyContainer = document.querySelector('.properties'); //get div element
+//array with user review info
 var reviews = [
     {
         name: 'Sheia',
@@ -27,10 +28,56 @@ var reviews = [
         date: '27-03-2021'
     },
 ];
-// functon that that show review tota;
-function showReviewTotal(value, reviewer, rating) {
-    isLoyal = rating;
-    var starRating = isLoyal ? '⭐' : '';
-    exports.reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + starRating;
-}
-showReviewTotal(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+//object with user info
+var you = {
+    firstName: 'Bobby',
+    lastName: 'Brown',
+    isReturning: true,
+    age: 35,
+    stayedAt: ['florida-home', 'oman-flat', 'tokyo-bungalow']
+};
+// Array of Properties
+var properties = [
+    {
+        image: 'images/colombia-property.jpg',
+        title: 'Colombian Shack',
+        price: 45,
+        location: {
+            firstLine: 'shack 37',
+            city: 'Bogota',
+            code: 45632,
+            country: 'Colombia'
+        },
+        contact: 'marywinkle@gmail.com',
+        isAvailable: true
+    },
+    {
+        image: 'images/poland-property.jpg',
+        title: 'Polish Cottage',
+        price: 34,
+        location: {
+            firstLine: 'no 23',
+            city: 'Gdansk',
+            code: 343903,
+            country: 'Poland'
+        },
+        contact: 'garydavis@hotmail.com',
+        isAvailable: false
+    },
+    {
+        image: 'images/london-property.jpg',
+        title: 'London Flat',
+        price: 23,
+        location: {
+            firstLine: 'flat 15',
+            city: 'London',
+            code: 35433,
+            country: 'United Kingdom',
+        },
+        contact: 'andyluger@aol.com',
+        isAvailable: true
+    }
+];
+// call the function
+(0, utils_1.showReviewTotal)(reviews.length, reviews[0].name, reviews[0].loyaltyUser);
+(0, utils_1.populateUser)(you.isReturning, you.firstName);
