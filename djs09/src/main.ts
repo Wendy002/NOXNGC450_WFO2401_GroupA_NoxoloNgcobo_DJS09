49939684ca08c0053a6f107b42d34093ed0577c8
Noxolo <wendyngcobo98@gmail.com>
2024-06-06 15:26:31 +0200

    change boolean values to loyaltyuser enums

--- djs09/src/main.ts
+++ djs09/src/main.ts
@@ -19,19 +19,19 @@ const reviews : {
     {
         name: 'Sheia',
         stars: 5,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.GOLD_USER,
         date: '01-04-2021'
     },
     {
         name: 'Andrzej',
         stars: 3,
-        loyaltyUser: false,
+        loyaltyUser: LoyaltyUser.BRONZE_USER,
         date: '28-03-2021'
     },
     {
         name: 'Omar',
         stars: 4,
-        loyaltyUser: true,
+        loyaltyUser: LoyaltyUser.SILVER_USER,
         date: '27-03-2021'
     },
 ]
