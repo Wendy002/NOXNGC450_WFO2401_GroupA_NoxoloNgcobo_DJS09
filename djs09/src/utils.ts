a403af6cb34a12acb1101d31f92d828f375ec327
Noxolo <wendyngcobo98@gmail.com>
2024-06-06 15:33:52 +0200

    replace boolean variable enum enum variable

--- djs09/src/utils.ts
+++ djs09/src/utils.ts
@@ -1,9 +1,11 @@
+import { LoyaltyUser } from "./enum.ts"
+
 const reviewTotalDisplay = document.querySelector('#reviews') as HTMLElement
 const returningUserDisplay = document.querySelector('#returning-user') as HTMLElement
 const userNameDisplay = document.querySelector('#user') as HTMLElement
 
-export function showReviewTotal(value: number, reviewer: string, isLoyalty: boolean) {
-    const iconDisplay = isLoyalty ? '⭐' : ''
+export function showReviewTotal(value: number, reviewer: string, isLoyalty: LoyaltyUser) {
+    const iconDisplay = LoyaltyUser.GOLD_USER ? '⭐' : ''
     reviewTotalDisplay.innerHTML = 'review total ' + value.toString() + '| last reviewed by ' + reviewer + ' ' + iconDisplay
 }
 
