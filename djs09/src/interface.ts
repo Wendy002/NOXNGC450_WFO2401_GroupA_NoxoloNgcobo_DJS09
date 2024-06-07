9e6e848204f89fc4b6d98bfb457441b08f2db8ba
Noxolo <wendyngcobo98@gmail.com>
2024-06-07 14:12:50 +0200

    create interface for properties

--- djs09/src/interface.ts
+++ djs09/src/interface.ts
@@ -1,12 +1,27 @@
+import { Price} from './types'
 enum LoyaltyUser {
     GOLD_USER = 'GOLD_USER',
     SILVER_USER = 'SILVER_USER',
     BRONZE_USER = 'BRONZE_USER'
 }
 
-export default interface Review {
+export interface Review {
     name: string;
     stars: number;
     loyaltyUser: LoyaltyUser;
     date: string;
+}
+
+export interface Property {
+    image: string;
+    title: string;
+    price: Price;
+    location: {
+        firstLine: string;
+        city: string;
+        code: number | string;
+        country: string
+    }
+    contact: [ number, string];
+    isAvailable: boolean;
 }
